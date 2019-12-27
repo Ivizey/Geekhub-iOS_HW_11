bdeb53c38e5ceeb271c57405d73be33fd7992080
Pavel Bondar <ivizey@gmail.com>
2019-12-27 15:57:05 +0200

    Initial Commit

---
+++ iOS_HW_11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_HW_11
+//
+//  Created by Pavel Bondar on 27.12.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
