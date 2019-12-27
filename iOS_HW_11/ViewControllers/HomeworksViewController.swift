9cd48c1e0a17371a30097dc0459857d9eb8a1486
Pavel Bondar <ivizey@gmail.com>
2019-12-27 16:22:27 +0200

    add view controllers & FetchedResultControllerDelegate

---
+++ iOS_HW_11/ViewControllers/HomeworksViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeworksViewController.swift
+//  iOS_HW_11
+//
+//  Created by Pavel Bondar on 27.12.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class HomeworksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
